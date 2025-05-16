90cd93dce75990fbfcbb6bb0d903c6c2a703af30
nikhil-kr20 <nikhilkumar868400@gmail.com>
2025-05-16 15:25:54 +0530

    Create script.js
    created this JS file.
    Also added for functioning the two button for viewing the projects in the same tab

---
+++ PortfolioWeb/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var viewAllBtn = document.getElementById("view-all-projects-btn1");
+    if (viewAllBtn) {
+        viewAllBtn.addEventListener("click", function() {
+            window.location.href = "recentProjects.html";
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var viewAllBtn = document.getElementById("view-all-projects-btn");
+    if (viewAllBtn) {
+        viewAllBtn.addEventListener("click", function() {
+            window.location.href = "recentProjects.html";
+        });
+    }
+});
