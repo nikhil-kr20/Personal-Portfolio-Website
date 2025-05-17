e08e05628b713864a388275bbbcdf6c72dd63f8d
nikhil-kr20 <nikhilkumar868400@gmail.com>
2025-05-17 15:54:51 +0530

    Update script.js
    added the alert text

--- PortfolioWeb/script.js
+++ PortfolioWeb/script.js
@@ -1,3 +1,4 @@
+alert("Welcome to my portfolio\nThis site is not fully completed yet & is still under construction.\n\nThankyou for visiting my Website.");
 document.addEventListener("DOMContentLoaded", function() {
     var viewAllBtn = document.getElementById("view-all-projects-btn1");
     if (viewAllBtn) {
